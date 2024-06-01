b8e3f807d140ca67395b7ff87e63083c34924ad5
Ground-Zerro <ksa65s571dozor@gmail.com>
2024-06-01 19:07:09 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import configparser
+import logging
 import socket
 import time
 from typing import Tuple, List, Optional
@@ -9,7 +10,10 @@ import telnetlib3
 from cachetools import TTLCache
 from dnslib import DNSRecord, DNSError
 
-# Простая реализация кэша для хранения IP-адресов и DNS имен с использованием TTLCache
+# Настройка логгирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Кэш для хранения IP-адресов и DNS имен
 ip_cache_data = TTLCache(maxsize=1000, ttl=10800)  # Кэш IP адресов с TTL 3 часа
 dns_cache_data = TTLCache(maxsize=1000, ttl=60)    # Кэш DNS имен с TTL 1 минута
 
@@ -19,14 +23,17 @@ def read_config(filename: str) -> Optional[configparser.SectionProxy]:
     config = configparser.ConfigParser()
     try:
         config.read(filename)
-        print(f"Файл конфигурации {filename} загружен.")
+        logging.info(f"Файл конфигурации {filename} загружен.")
         return config['RouterSyncNavigator']
+    except KeyError:
+        logging.error(f"Секция 'RouterSyncNavigator' отсутствует в файле конфигурации {filename}.")
+        return None
     except Exception as e:
-        print(f"Ошибка загрузки файла конфигурации {filename}: {e}")
+        logging.error(f"Ошибка загрузки файла конфигурации {filename}: {e}")
         return None
 
 
-# Асинхронная функция для отправки DNS запросов к публичному DNS серверу
+# Функция для отправки DNS запросов к публичному DNS серверу
 async def send_dns_query(data: bytes, public_dns: str) -> Optional[bytes]:
     loop = asyncio.get_event_loop()
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -36,16 +43,16 @@ async def send_dns_query(data: bytes, public_dns: str) -> Optional[bytes]:
         response, _ = await loop.run_in_executor(None, client_socket.recvfrom, 1024)
         return response
     except socket.timeout:
-        print(f"Тайм-аут при отправке DNS запроса к {public_dns}")
+        logging.warning(f"Тайм-аут при отправке DNS запроса к {public_dns}")
         return None
     except Exception as e:
-        print(f"Ошибка отправки DNS запроса: {e}")
+        logging.error(f"Ошибка отправки DNS запроса: {e}")
         return None
     finally:
         client_socket.close()
 
 
-# Функция обработки полученного ответа
+# Функция обработки ответа от DNS сервера
 def process_dns_response(dns_response: bytes) -> Tuple[str, List[str]]:
     resolved_addresses = []
     domain = ""
@@ -55,9 +62,9 @@ def process_dns_response(dns_response: bytes) -> Tuple[str, List[str]]:
             if r.rtype == 1:  # A record
                 resolved_addresses.append(str(r.rdata))
                 domain = str(r.rname)
-                print(f"Resolved {r.rname} to {r.rdata}")
+                logging.info(f"Resolved {r.rname} to {r.rdata}")
     except DNSError as e:
-        print(f"Ошибка обработки DNS ответа: {e}")
+        logging.error(f"Ошибка обработки DNS ответа: {e}")
     return domain, resolved_addresses
 
 
@@ -84,25 +91,23 @@ def compare_dns(f_domain: str, domain_file: str) -> bool:
                 if match:
                     return True
     except Exception as e:
-        print(f"Ошибка сравнения DNS: {e}")
+        logging.error(f"Ошибка сравнения DNS: {e}")
     return False
 
 
 # SSH только для keenetic CLI
-async def send_commands_via_ssh(router_ip: str, ssh_port: int, login: str,
-                                password: str, commands: List[str]) -> None:
+async def send_commands_via_ssh(router_ip: str, ssh_port: int, login: str, password: str, commands: List[str]) -> None:
     try:
-        async with (asyncssh.connect(router_ip, port=ssh_port, username=login, password=password, known_hosts=None)
-                    as conn):
-            for command in commands:
-                result = await conn.run(command)
-                if result.stderr:
-                    print(result.stderr, end='')
+        conn = await asyncssh.connect(router_ip, port=ssh_port, username=login, password=password, known_hosts=None)
+        for command in commands:
+            result = await conn.run(command)
+            if result.stderr:
+                logging.error(result.stderr, end='')
     except asyncssh.Error as e:
-        print(f"Ошибка при выполнении команд через SSH: {e}")
+        logging.error(f"Ошибка при выполнении команд через SSH: {e}")
 
 
-# Telnet только для keenetic CLI
+# Telnet только для keenetic CLI ==ПРОВЕРИТЬ!! Возможно поломал пока ковырял SSH...==
 async def send_commands_via_telnet(router_ip: str, router_port: int, login: str, password: str,
                                    commands: List[str]) -> None:
     try:
@@ -127,7 +132,7 @@ async def send_commands_via_telnet(router_ip: str, router_port: int, login: str,
             writer.write(command)
             writer.write('\r\n')
     except Exception as e:
-        print(f"Ошибка при выполнении команд через Telnet: {e}")
+        logging.error(f"Ошибка при выполнении команд Telnet: {e}")
 
 
 # Функция кэширования IP-адресов для снижения частоты обращения к роутеру
@@ -152,15 +157,16 @@ async def main() -> None:
         public_dns = config_data['public_dns']
         server_ip = config_data['server_ip']
         server_port = int(config_data['server_port'])
+
     except KeyError as e:
-        print(f"Ошибка чтения параметров конфигурации: отсутствует ключ {e}")
+        logging.error(f"Ошибка чтения параметров конфигурации: отсутствует ключ {e}")
         return
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.settimeout(10)  # Устанавливаем тайм-аут на 10 секунд для сервера
+    server_socket.settimeout(10)  # Тайм-аут  для сервера
     loop = asyncio.get_event_loop()
     server_socket.bind((server_ip, server_port))
-    print(f'DNS Server listening on {server_ip}:{server_port}')
+    logging.info(f'DNS сервер запущен {server_ip}:{server_port}')
 
     async def handle_client(data: bytes, client_address: Tuple[str, int]) -> None:
         dns_response = await send_dns_query(data, public_dns)
@@ -172,15 +178,15 @@ async def main() -> None:
                 for address in resolved_addresses:
                     if not ip_cache(address.rstrip('.')):
                         commands = [f"ip route {address.rstrip('.')}/32 {eth_id}" for address in resolved_addresses]
-                        commands.append("exit")
-                        print(f"{f_domain} найден в фильтре, добавляем маршрут")
+                        logging.info(f"домен {f_domain} найден в фильтре - добавляем маршрут для него")
                         ip_cache_data[address.rstrip('.')] = time.time()
                         if connection_type == 'ssh':
                             await send_commands_via_ssh(router_ip, router_port, login, password, commands)
                         elif connection_type == 'telnet':
                             await send_commands_via_telnet(router_ip, router_port, login, password, commands)
                     else:
-                        print(f"Маршрут к {f_domain} был добавлен ранее")
+                        remaining_ttl = int((ip_cache_data[address.rstrip('.')] + 10800 - time.time()) / 60)
+                        logging.info(f"Маршрут к {f_domain} есть в кэше, оставшееся время жизни: {remaining_ttl} минут")
 
     async def recvfrom_loop():
         while True:
@@ -188,9 +194,9 @@ async def main() -> None:
                 data, client_address = await loop.run_in_executor(None, server_socket.recvfrom, 1024)
                 asyncio.create_task(handle_client(data, client_address))
             except socket.timeout:
-                print("Тайм-аут при ожидании данных от клиента")
-            except Exception as e:
-                print(f"Ошибка в основной петле: {e}")
+                continue
+            except Exception as E:
+                logging.error(f"Ошибка в основной петле: {E}")
 
     try:
         await recvfrom_loop()
